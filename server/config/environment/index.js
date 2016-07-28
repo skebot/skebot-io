94beb1d668c90b4d4a59b3d08f7c4f0b2b91f7de
skebot19 <skebot19@gmail.com>
2016-07-28 09:14:02 +0530

    env Change

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -44,7 +44,8 @@ var all = {
   facebook: {
     clientID:     process.env.FACEBOOK_ID || 'id',
     clientSecret: process.env.FACEBOOK_SECRET || 'secret',
-    callbackURL:  (process.env.DOMAIN || '') + '/auth/facebook/callback'
+    //callbackURL:  (process.env.DOMAIN || '') + '/auth/facebook/callback'
+    callbackURL:  'https://skebot-io.herokuapp.com/auth/facebook/callback'
   },
 
   twitter: {
